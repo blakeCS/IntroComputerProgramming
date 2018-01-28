66cb2ed0d04b130641f79da56e07a8e5b75e8a3f
blakeCS <35789943+blakeCS@users.noreply.github.com>
2018-01-28 11:25:37 -0600

    Update Comments

--- U2 Methods/Method Anatomy/1.2 Exercises/Add10Tester.java
+++ U2 Methods/Method Anatomy/1.2 Exercises/Add10Tester.java
@@ -2,16 +2,18 @@ import org.junit.*;
 import java.io.*;
 
 /**
- * A JUnit test case class.
- * Every method starting with the word "test" will be called when running
- * the test with JUnit.
+ * A JUnit test case class for Add10 Exercise.
+ * @author Madeline Burton
+ * @version 2.0
+ * @since 1.28.18
  */
 public class Add10Tester
 {
   
   private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
   private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-  
+
+  //collect all output in to a list
   @Before
   public void setUpStreams() {
     System.setOut(new PrintStream(outContent));
@@ -24,21 +26,17 @@ public class Add10Tester
     System.setErr(System.err);
   }
   
-  /**
-   * A test method.
-   * (Replace "X" with a name describing the test.  You may write as
-   * many "testSomething" methods in this class as you wish, and each
-   * one will be called when running JUnit over this class.)
-   */
   @Test
   public void testAdd10() 
   {
     Add10 a = new Add10();
     a.run();
     
+    //Separate output in to a string array
     String output = outContent.toString();
     String[] lines = output.split("\n");
     
+    //Check each element in array
     Assert.assertEquals("11", lines[0]);
     Assert.assertEquals("110", lines[1]);
     Assert.assertEquals("-40", lines[2]);
