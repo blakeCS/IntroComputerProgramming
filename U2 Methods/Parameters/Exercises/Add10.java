b0b24910ec586e56899f285167b8b62e441d3108
blakeCS <35789943+blakeCS@users.noreply.github.com>
2018-01-28 14:28:03 -0600

    Update and rename U2 Methods/Parameters/Add10.java to U2 Methods/Parameters/Exercises/Add10.java

--- U2 Methods/Parameters/Exercises/Add10.java
+++ U2 Methods/Parameters/Exercises/Add10.java
@@ -1,3 +1,11 @@
+/** This class is an exercise to be completed.  Edit the code only in the method add10 such that it adds 10 to the value a
+  * and prints the result to the console. 
+  *
+  * 
+  * @author Madeline Burton
+  * @version 1.0
+  * @since 1.24.2018
+  */
 public class Add10
 {
     public void run()
@@ -7,6 +15,8 @@ public class Add10
         add10(-50);
     }
     
+    /** Print a + 10 to the console
+    * @param a: any int value -infinity to infinity) */
     private void add10(int a)
     {
         // Fill this in to print the result to the console
