28babff4b4766f208f593524e051c8417ff191fc
blakeCS <35789943+blakeCS@users.noreply.github.com>
2018-01-28 15:54:05 -0600

    Create Madlib.java

---
+++ U2 Methods/Parameters/Exercises/Madlib.java
@@ -0,0 +1,42 @@
+
+/** This class is an exercise to be completed.  Edit the code only in the method add10 such that it adds 10 to the value a
+  * and prints the result to the console. 
+  *
+  * 
+  * @author Madeline Burton
+  * @version 1.0
+  * @since 1.24.2018
+  */
+public class Madlib
+{
+    public void run()
+    {
+        /* Add the method calls to print two madlib statements. The first should include:
+         * person: fairy godmother
+         * animal: cheetah
+         * number: 5
+         * verb: run
+         * place: Paris
+         * noun: popsicle
+         */
+    }
+    
+
+
+    /** Creates a madlib using the sentence:
+      *     My <person> says I can't have a <animal> until I am <number>
+      *     years old and learn to <verb> my <place> and take out the <noun>.
+      * COMPLETE @PARAM STATEMENTS
+      */
+    private void constructMadlib(  /*include your parameters*/  ) {
+        
+        // Fill this in to print the result to the console
+      
+    }
+    
+//Ignore this code-----------------------------------------------------------------------------------------------
+  public static void main(String args[]){
+  Madlib m = new Madlib();
+  m.run();
+  }
+} 
