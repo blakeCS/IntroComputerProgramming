77e16a65aa73e6baa419d7e63caf24d4637647fd
blakeCS <35789943+blakeCS@users.noreply.github.com>
2018-01-28 11:57:43 -0600

    Update Add10.java

--- U2 Methods/Parameters/Add10.java
+++ U2 Methods/Parameters/Add10.java
@@ -15,7 +15,7 @@ public class Add10
 //Ignore this code-----------------------------------------------------------------------------------------------
   public static void main(String args[]){
   Add10 a = new Add10();
-  l.run();
+  a.run();
   }
 } 
 
