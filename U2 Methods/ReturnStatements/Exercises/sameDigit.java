3527bbcc2e2537de6c88b637cd37264cb67bb33c
blakeCS <35789943+blakeCS@users.noreply.github.com>
2018-01-29 15:20:07 -0600

    Create sameDigit.java

---
+++ U2 Methods/ReturnStatements/Exercises/sameDigit.java
@@ -0,0 +1,42 @@
+/** This class is an exercise to be completed.  Edit the code only in the method sameDigit such that 
+  * Given two non-negative int values, return true if they have the same last digit, \
+  * such as with 27 and 57, and false otherwise.
+  * Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+  * 
+  * Examples:
+  * lastDigit(7, 17) → true
+  * lastDigit(6, 17) → false
+  * lastDigit(3, 113) → true
+  * 
+  * 
+  * @author YOUR NAME
+  * @version 1.0
+  * @since 1.29.2018
+  */
+
+public class Compare {
+  
+  //Run the tests on three examples and check that they work correctly.
+  public void run(){
+    
+    System.out.println("Test sameDigit");
+    System.out.println(sameDigit(24, 3));   // false
+    System.out.println(sameDigit(5, 15));   // true
+    System.out.println(sameDigit(342, 22)); // true
+    
+  }
+  
+  //Write your code here.  Create the whole method statement includng the signature and the methody body.
+  //Method: sameDigit
+
+  }
+  
+  
+  //Ignore this code-----------------------------------------------------------------------------------------------
+  public static void main(String args[]){
+    Compare a = new Compare();
+    a.run();
+  }
+  
+}
+  
