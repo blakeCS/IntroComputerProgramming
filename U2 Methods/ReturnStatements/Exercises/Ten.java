0f33c25e1a839447c5a443e721a31466643b1f25
blakeCS <35789943+blakeCS@users.noreply.github.com>
2018-01-30 14:06:46 -0600

    Create Ten.java

---
+++ U2 Methods/ReturnStatements/Exercises/Ten.java
@@ -0,0 +1,44 @@
+/** This class is an exercise to be completed.  Edit the code only in the method makes10 such that 
+  * Given 2 ints, a and b, it returns true if one if them is 10 or if their sum is 10.
+  * 
+  * Examples:
+  * makes10(9, 10) → true
+  * makes10(9, 9) → false
+  * makes10(1, 9) → true
+  * 
+  * 
+  * @author YOUR NAME
+  * @version 1.0
+  * @since DATE
+  */
+
+public class Ten {
+  
+  //Run the tests on three examples and check that they work correctly.
+  public void run(){
+    
+    System.out.println("Test sameDigit");
+    System.out.println(makes10(9,10));   // true
+    System.out.println(makes10(5, 5));   // true
+    System.out.println(makes10(2, 5)); // false
+    
+  }
+  
+  //Write your code here.  Create the whole method statement includng the signature and the methody body.
+  //Method: makes10
+  public boolean makes10(int a, int b){
+    boolean x = (a == 10);
+    boolean y = (b == 10);
+    boolean z = ((a+b) == 10);
+    return x || y || z;
+  }
+
+  
+  //Ignore this code-----------------------------------------------------------------------------------------------
+  public static void main(String args[]){
+    Ten a = new Ten();
+    a.run();
+  }
+  
+}
+  
