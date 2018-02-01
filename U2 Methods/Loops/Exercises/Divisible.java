07b287ed794b5a1ed056ce4457c9a67fc134a304
blakeCS <35789943+blakeCS@users.noreply.github.com>
2018-02-01 17:51:54 -0600

    Create Divisible.java

---
+++ U2 Methods/Loops/Exercises/Divisible.java
@@ -0,0 +1,48 @@
+/** Complete this program such that it accepts an integer parameter, and all numbers between 1 and 100.  For each,
+  * Identify whether the number is evently divisible by the parameter.
+  * 
+  * Note:  To pass the test case, your print statement must match the statements in the example EXACTLY.
+  * 
+  * Example:
+  * isDivisible(5)
+  * 
+  * ----------------
+  * 1: false
+  * 2: false
+  * 3: false
+  * 4: false
+  * 5: true
+  * 6: false
+  * etc.
+  * 
+  * @author YOUR NAME
+  * @version 1.0
+  * @since DATE
+  */
+
+public class Divisible{
+  
+  public void run(){
+  
+    // Test your program by running it with a few different parameters, 
+    // but make sure you use isDivisible(5) when running the test case.
+    isDivisible(5);
+    
+  }
+  
+  
+  //Complete the method below
+  public void isDivisible(int n){
+    
+    
+  }
+  
+  
+  //Ignore this code-----------------------------------------------------------------------------------------------
+  public static void main(String args[]){
+    Divisible test = new Divisible();
+    test.run();
+  
+  }
+}
+
