2f38e985a673516c530509c99602afccebc6b25a
blakeCS <35789943+blakeCS@users.noreply.github.com>
2018-02-01 21:26:41 -0600

    Create CreateMod.java

---
+++ U2 Methods/Loops/Exercises/CreateMod.java
@@ -0,0 +1,47 @@
+/** Design a function that takes in parameters a and b and returns a % b using loops.  You may NOT use the % operation.
+  * Remember, modulus is the remainder when performing a / b.
+  * 
+  * Examples:
+  * modulus(10,3)  --> 1
+  * modulus(5,2)   --> 1
+  * modulus(15,4)  --> 3
+  * modulus(18, 6) --> 0
+  * 
+  * 
+  * @author YOUR NAME
+  * @version 1.0
+  * @since DATE
+  */
+
+public class CreateMod{
+    public void run()
+    {
+      //Test modulus
+      int a = modulus(5, 3);
+      int b = modulus(38, 2);
+      int c = modulus(100, 7);
+      
+      //YOUR CODE HERE ----- Write print statements to make sure a, b and c contain the corect value.
+      
+      
+
+      
+    }
+    
+    /** Yields: The remainder of a / b.
+      * @param a: The value being divided (numerator) 
+      * @param b: The amount of equal portions (divisor)
+      * 
+      * YOUR CODE HERE ---- Write the code for the function modulus such that it meets the above specifications.
+      */
+
+
+
+
+      //Ignore this code-----------------------------------------------------------------------------------------------
+  public static void main(String args[]){
+  CreateMod test = new CreateMod();
+  test.run();
+  }
+
+}
