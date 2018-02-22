6fb4e029570e519638869618ccf83d13a5362c88
blakeCS <35789943+blakeCS@users.noreply.github.com>
2018-02-22 09:31:32 -0600

    Create PasswordChecker.java

---
+++ U2 Methods/Strings/PasswordChecker.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class passwordChecker here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PasswordChecker
+{
+    // instance variables - replace the example below with your own if necessary
+    private int x;
+
+    /**
+     * A main RUN method for password checker 
+     */
+    public void run(){
+       //Test your base method here to see your output
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y){
+        // put your code here
+        return x + y;// you should probably erase this and return something that is valuable to you.
+    }
+    
+    //Ignore this Code --------------------------------
+    public static void main(String args[]){
+     PasswordChecker check = new PasswordChecker();
+     check.run();
+    }
+}
