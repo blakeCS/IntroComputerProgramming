3f1af140243d89d6cc3276315117137d07315936
blakeCS <35789943+blakeCS@users.noreply.github.com>
2018-02-21 21:12:13 -0600

    Create Palindromes.java

---
+++ U2 Methods/Strings/Palindromes.java
@@ -0,0 +1,60 @@
+public class Palindromes{
+  /**
+   * This program lets the user input some text and
+   * prints out whether or not that text is a palindrome.
+   */
+  public void run(){
+    //Change this string to test whether your palindrome function works.
+    String text = "kayak";
+    Boolean x = isPalindrome(text);
+    toUser(x);
+  }
+  
+  public void toUser(boolean isSame){
+    if(isSame){
+      System.out.println("Your word is a palindrome!");
+    } else {
+      System.out.println("Not a palindrome :(");
+    }
+  }
+  /**
+   * This method determines if a String is a palindrome,
+   * which means it is the same forwards and backwards.
+   * 
+   * @param text The text we want to determine if it is a palindrome.
+   * @return A boolean of whether or not it was a palindrome.
+   */
+  private boolean isPalindrome(String text)
+  {
+    String reversed = reverse(text);
+    if(text.equals(reversed))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  
+  /**
+   * This method reverses a String.
+   * 
+   * @param text The string to reverse.
+   * @return The new reversed String.
+   */
+  private String reverse(String text) {
+    String result = "";
+    for(int i = text.length() - 1; i >= 0; i--)
+    {
+      char cur = text.charAt(i);
+      result += cur;
+    }
+    return result;
+  }
+  
+  public static void main (String args[]){
+  Palindromes p = new Palindromes();
+  p.run();
+  }
+}
